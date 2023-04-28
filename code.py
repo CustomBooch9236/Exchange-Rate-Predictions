7ffeb4526f7c6503d73b625cc34772151491e3a3
Sumit Chaturvedi <119087961+CustomBooch9236@users.noreply.github.com>
2023-04-28 03:21:13 +0100

    Update code.py

--- code.py
+++ code.py
@@ -9,33 +9,39 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-# Set parameters from config file
+# Yahoo Finance API parameters
 symbol = config['YahooFinance']['symbol']
 start_date = config['YahooFinance']['start_date']
 end_date = config['YahooFinance']['end_date']
+
+# Machine learning model parameters
 test_size = float(config['Model']['test_size'])
 shuffle_data = bool(config['Model']['shuffle_data'])
 
 # Download exchange rate data from Yahoo Finance
 df = yf.download(symbol, start=start_date, end=end_date)
 
-# Adding a new column with the lagged exchange rate
-df.loc[:, 'Lagged_Rate'] = df['Exchange_Rate'].shift(1)
+# Create a new DataFrame with only the exchange rate column and date index
+df = df.loc[:, ['Close']]
+df.index.names = ['Date']
+df.columns = ['Exchange_Rate']
 
-# Removing the first row since it doesn't have a lagged value
-df = df.iloc[1:]
+# Add a new column with the lagged exchange rate
+df['Lagged_Rate'] = df['Exchange_Rate'].shift(1)
 
-# Splitting the data into training and testing sets
-X = df[['Lagged_Rate']]
-y = df[['Exchange_Rate']]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+# Drop rows with missing values
+df.dropna(inplace=True)
 
-# Creating a linear regression model
-lr = LinearRegression()
-lr.fit(X_train, y_train)
+# Split the data into training and testing sets
+X = df.loc[:, ['Lagged_Rate']]
+y = df.loc[:, 'Exchange_Rate']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=shuffle_data)
 
-# Predicting the exchange rate
-predicted_rate = lr.predict(X_test)
+# Fit a linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
 
-# Printing the predicted rate
-print(predicted_rate[-1])
+# Predict the exchange rate
+last_exchange_rate = df.iloc[-1]['Exchange_Rate']
+predicted_exchange_rate = model.predict([[last_exchange_rate]])
+print(f'Predicted exchange rate: {predicted_exchange_rate[0]:.2f}')
